7c98a5e6f36fe6630878b89eb18399ced62f4642
Bertrand Roussel <broussel@sierrawireless.com>
2017-05-01 10:22:59 -0700

    Remove runtime tool install/uninstall

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,4 @@
 FROM sstarcher/sensu
 
-RUN ( \
-        cat /etc/os-release && \
-        apt-get update && \
-        apt-get install -yy libxml2 libxml2-dev libxslt1-dev zlib1g-dev build-essential lm-sensors && \
-        gem install unf_ext && \
-        ln -s ld-linux-x86-64.so.2 /lib64/ld-lsb-x86-64.so.3 \
-    )
+ADD client-install.sh /client-install.sh
+RUN /client-install.sh
---
+++ client-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -xe
+
+cat /etc/os-release
+
+# Remove tool runtime install/uninstall as packages are shipped
+# with this image.
+sed -i 's/tooling_\(un\)\{0,1\}install$//g' /bin/install
+
+# Install tools
+apt-get update
+apt-get install -yy libxml2 libxml2-dev libxslt1-dev zlib1g-dev build-essential lm-sensors
+gem install unf_ext
+ln -s ld-linux-x86-64.so.2 /lib64/ld-lsb-x86-64.so.3
