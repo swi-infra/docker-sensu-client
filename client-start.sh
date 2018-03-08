6c494ce9abb8f107d2bdcd99c59468e099447535
Bertrand Roussel <broussel@sierrawireless.com>
2018-03-08 10:35:57 -0800

    Use iperf3
    Resolves: FARM-1517
    Change-Id: Iab92dde2bcf01424da6261d8c67fdfbf33bffd2b

--- client-start.sh
+++ client-start.sh
@@ -4,7 +4,7 @@ IPERF_SERVER=${IPERF_SERVER:-false}
 
 if [[ "$IPERF_SERVER" == "true" ]]; then
     echo "Serving iperf ..."
-    iperf -s $IPERF_SERVER_OPTS &
+    iperf3 -s $IPERF_SERVER_OPTS &
 fi
 
 /bin/start client
