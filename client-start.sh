062ca5335f054fea9b8918e97322e969c0c967e1
Bertrand Roussel <broussel@sierrawireless.com>
2019-06-06 11:14:02 -0700

    Add optional ceph dependency
    Resolves: FARM-2874
    Change-Id: I198567d92b5f29bf7a81e945661155fc80447700

--- client-start.sh
+++ client-start.sh
@@ -7,4 +7,9 @@ if [[ "$IPERF_SERVER" == "true" ]]; then
     iperf3 -s $IPERF_SERVER_OPTS &
 fi
 
+if [[ "$CLIENT_SUBSCRIPTIONS" == *ceph* ]]; then
+    apt-get update
+    apt-get install -yy ceph
+fi
+
 /bin/start client
